d4cbaed27788593f6b6a3a0e076ec5339be6d124
hokin2023 <hokin990@gmail.com>
2023-06-21 17:48:06 -0700

    added stuff

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_PASSWORD,
    
     {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
@@ -20,3 +20,4 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = sequelize;
+// 127.0.0.1
